39b4b3005c9ad839e1ac4c730d464cffc9d3d3a7
madihamulla134-beep <madihamulla134@gmail.com>
2026-08-10 16:31:19 +0530

    connect fast API to ASL model

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+from PIL import Image
+import io
+import torch
 
 app = FastAPI(title="MR Voxa API")
+
+# ASL model location
+MODEL_PATH = "../asl_model"
+
+# Load model once when the backend starts
+processor = AutoImageProcessor.from_pretrained(MODEL_PATH)
+model = AutoModelForImageClassification.from_pretrained(MODEL_PATH)
+
+model.eval()
 
 
 @app.get("/")
@@ -19,7 +32,41 @@ def health():
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
+
+    # Read uploaded image
+    image_bytes = await file.read()
+
+    # Convert image to RGB
+    image = Image.open(
+        io.BytesIO(image_bytes)
+    ).convert("RGB")
+
+    # Prepare image
+    inputs = processor(
+        images=image,
+        return_tensors="pt"
+    )
+
+    # Run ASL model
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # Convert model output to probabilities
+    probabilities = torch.softmax(
+        outputs.logits,
+        dim=-1
+    )[0]
+
+    # Find highest probability
+    predicted_id = probabilities.argmax().item()
+
+    # Get ASL letter
+    predicted_label = model.config.id2label[predicted_id]
+
+    # Get confidence
+    confidence = probabilities[predicted_id].item()
+
     return {
-        "message": "Image received successfully",
-        "filename": file.filename
+        "sign": predicted_label,
+        "confidence": confidence
     }
